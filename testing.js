41e34922f0d3887b7aa483fb2a0e439e313af044
Anna Lian <alian10@terpmail.umd.edu>
2025-04-08 14:41:11 -0400

    created a page for api testing

---
+++ testing.js
@@ -0,0 +1,9 @@
+// API Testing
+//API Mapping - 20 pts
+//Create a List of the Pages you are going to Create and what data you will show
+//List the API’s which you will leverage to show the data on the page
+//Explain what the API does
+//API Results - 30 pts
+//Make Insomnia Calls to the API(s) that you will use
+//Show that you are able to get the data you desire
+//Take Screenshots of the Request + Response showing the data for each API
