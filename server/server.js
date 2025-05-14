28b413211882db4560af61edbfb2bef15dacb05f
Anna Lian <alian10@terpmail.umd.edu>
2025-05-14 01:35:02 -0400

    fixed typo

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const animalRoutes = require('./routes/animalRoutes');
+const animeRoutes = require('./routes/animeRoutes');
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/animals', animalRoutes);
+app.use('/api/anime', animeRoutes);
 
 app.get('/', (req, res) => {
     res.send('API is running!');
