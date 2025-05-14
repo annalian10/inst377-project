33e4923d56d926abab09f6c23252dd0d57e484ad
Anna Lian <alian10@terpmail.umd.edu>
2025-05-14 01:20:24 -0400

    started the backend stuff

--- server/routes/animeRoutes.js
+++ server/routes/animeRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { getRecommendations, saveWatchlist } = require('../controllers/animeController'); 
 
+
+router.get('/recommendations', getRecommendations);
+router.post('/watchlist', saveWatchlist);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
 
+const express = required('express');
+const cors = required('cors');
+const dotenv = required('dotenv');
+const animalRoutes = required('./routes/animalRoutes');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/animals', animalRoutes);
+
+app.get('/', (req, res) => {
+    res.send('API is running!');
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
