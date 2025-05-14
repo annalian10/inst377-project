4ca628897ad1c3c58d802492b5f3b1ebfe329675
Anna Lian <alian10@terpmail.umd.edu>
2025-05-14 01:27:17 -0400

    added lines to animeController.js

--- server/controllers/animeController.js
+++ server/controllers/animeController.js
@@ -1,1 +1,32 @@
+const fetch = require('node-fetch');
+const supabase = require('../db/supabaseClient');
 
+// GET anime recommendations
+const getRecommendations = async (req, res) => {
+    try {
+        const response = await fetch('https://api.jikan.moe/v4/anime?q=naruto');
+        const data = await response.json();
+        res.json(data.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch recommendations' });
+    }
+};
+
+// POST to watchlist
+const saveWatchlist = async (req, res) => {
+    const { title, userId} = req.body;
+
+        const { data, error } = await supabase
+            .from('watchlist')
+            .insert([{ title, user_id: userId }]);
+
+        if (error) {
+           console.error(error);
+           return res.status(500).json({ error: 'Failed to save watchlist item' });
+        }
+
+        res.status(200).json({ message: 'Saved!', data});
+};
+
+module.exports = { getRecommendations, saveWatchlist };
